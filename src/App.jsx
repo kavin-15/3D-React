20c860764792bd06189d8c78e7c4bd133ef83980
kavin-15 <kavin_1580@yahoo.com>
2024-11-23 21:17:14 -0800

    Trying to use scroll control

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
 import React from 'react'
 import { Canvas } from '@react-three/fiber'
 import "./style.css"
-import { Environment, OrbitControls } from '@react-three/drei'
+import { Environment, OrbitControls, ScrollControls } from '@react-three/drei'
 import MacContainer from './MacContainer'
 
 function App() {
   return (
-    <Canvas camera={{fov: 20, positon:[0, -2, 120]}}>
+    <Canvas camera={{fov: 50, positon:[0, -80, 120]}}>
       <OrbitControls/>
       <Environment files={['https://dl.polyhaven.org/file/ph-assets/HDRIs/exr/4k/studio_small_09_4k.exr']}/>
+      <ScrollControls pages={2} damping={6}>
       <MacContainer/>
+      </ScrollControls>
       </Canvas>
   )
 }
--- src/MacContainer.jsx
+++ src/MacContainer.jsx
@@ -1,10 +1,24 @@
-import { useGLTF } from '@react-three/drei'
+import { useGLTF, useScroll } from '@react-three/drei'
+import { useFrame } from '@react-three/fiber';
 import React from 'react'
+import * as THREE from "three";
+
 
 function MacContainer() {
     let model = useGLTF("./mac.glb");
-  return(     
+    let meshes = {};
+    model.scene.traverse((e)=>{
+            meshes[e.name]=e;
+    } );
+    meshes.screen.rotation.x = THREE.MathUtils.degToRad(180);
+    let data = useScroll();
+    useFrame((state,delta)=>{
+        meshes.screen.rotation.x=THREE.MathUtils.degToRad(180-data.offset*90);
+    })
+  return( 
+    <group position={[0,-10,-40]}>
         <primitive object={model.scene}/>
+    </group>    
   )
 }
 
