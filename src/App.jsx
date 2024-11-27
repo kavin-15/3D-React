1d76d30d3cdab92101b744d55bcff2d1f7dd11c6
kavin-15 <kavin_1580@yahoo.com>
2024-11-26 22:57:08 -0800

    screen graphic, scroll fix and position adjustment.

--- src/App.jsx
+++ src/App.jsx
@@ -6,13 +6,18 @@ import MacContainer from './MacContainer'
 
 function App() {
   return (
-    <Canvas camera={{fov: 12, position:[0, -2, 120]}}>
-      <OrbitControls/>
+    <div className='w-full h-screen'>
+      <div className="absolute text-white top-32 left-1/2 -translate-x-1/2 font-['Helvetica_Now_Display']">
+        <h2 className='text-7xl tracking tighter'>MacBook Pro</h2>
+      </div>
+      <Canvas camera={{fov: 12, position:[0, -10, 220]}}>
       <Environment files={['https://dl.polyhaven.org/file/ph-assets/HDRIs/exr/4k/studio_small_09_4k.exr']}/>
       <ScrollControls pages={2} damping={6}>
       <MacContainer/>
       </ScrollControls>
       </Canvas>
+    </div>
+    
   )
 }
 
--- src/MacContainer.jsx
+++ src/MacContainer.jsx
@@ -1,4 +1,4 @@
-import { useGLTF, useScroll } from '@react-three/drei'
+import { useGLTF, useScroll, useTexture } from '@react-three/drei'
 import { useFrame } from '@react-three/fiber';
 import React from 'react'
 import * as THREE from "three";
@@ -6,14 +6,20 @@ import * as THREE from "three";
 
 function MacContainer() {
     let model = useGLTF("./mac.glb");
+    let tex = useTexture("./red.jpg");
     let meshes = {};
     model.scene.traverse((e)=>{
             meshes[e.name]=e;
     } );
     meshes.screen.rotation.x = THREE.MathUtils.degToRad(180);
+    meshes.matte.material.map = tex;
+    meshes.matte.material.emissiveIntensity = 0;
+    meshes.matte.material.metalness = 0;
+    meshes.matte.material.roughness = 1;
     let data = useScroll();
+    const speedMultiplier = 2;
     useFrame((state,delta)=>{
-        meshes.screen.rotation.x=THREE.MathUtils.degToRad(180-data.offset*90);
+        meshes.screen.rotation.x=THREE.MathUtils.degToRad(180-data.offset*90 * speedMultiplier);
     })
   return( 
     <group position={[0,-10,-40]}>
