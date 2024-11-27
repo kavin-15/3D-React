ca8efc5e6b8b59dc42141343ec5bd9a0bae50548
kavin-15 <kavin_1580@yahoo.com>
2024-11-26 23:58:56 -0800

    Navbar added

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,33 @@
 import React from 'react'
 import { Canvas } from '@react-three/fiber'
 import "./style.css"
-import { Environment, OrbitControls, ScrollControls } from '@react-three/drei'
+import { Environment, ScrollControls } from '@react-three/drei'
 import MacContainer from './MacContainer'
 
 function App() {
   return (
-    <div className='w-full h-screen'>
-      <div className="absolute text-white top-32 left-1/2 -translate-x-1/2 font-['Helvetica_Now_Display']">
-        <h2 className='text-7xl tracking tighter'>MacBook Pro</h2>
+    <div className='w-full h-screen relative'>
+      <div className='navbar flex gap-10 py-5 absolute top-10 left-1/2 -translate-x-1/2 z-20'>
+        {["iPhone", "iPad", "Mac", "Accessories", "Contact Us"].map((item, index) => (
+          <a
+            href="#"
+            key={index}
+            className="text-white font-[400] text-sm hover:underline"
+          >
+            {item}
+          </a>
+        ))}
       </div>
-      <Canvas camera={{fov: 12, position:[0, -10, 220]}}>
-      <Environment files={['https://dl.polyhaven.org/file/ph-assets/HDRIs/exr/4k/studio_small_09_4k.exr']}/>
-      <ScrollControls pages={2} damping={6}>
-      <MacContainer/>
-      </ScrollControls>
+      <div className="absolute text-white top-40 left-1/2 -translate-x-1/2 font-['Helvetica_Now_Display'] z-10">
+        <h2 className='text-7xl tracking-tight'>MacBook Pro</h2>
+      </div>
+      <Canvas camera={{ fov: 12, position: [0, -10, 220] }}>
+        <Environment files={['https://dl.polyhaven.org/file/ph-assets/HDRIs/exr/4k/studio_small_09_4k.exr']} />
+        <ScrollControls pages={2} damping={6}>
+          <MacContainer />
+        </ScrollControls>
       </Canvas>
     </div>
-    
   )
 }
 
